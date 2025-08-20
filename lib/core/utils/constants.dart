9f3de5ea3e147a75bf87574a4b71614db9473492
KimJihay <eh202201029@wmsu.edu.ph>
2025-08-20 20:59:17 +0800

    add color palette

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+//Color Palette
+const kPrimaryColor = Color(0xFF03457F);
+
+const kSecondaryColor = Color(0xFF009BDD);
+
+const kSecondaryColorAlt = Color.fromRGBO(0, 155, 221, 0.5);
+
+const kAccentColor = Color(0xFF6FC5C8);
+
+const kForegroundColor = Color(0xFF05121D);
+
+const kBackgroundColor = Color(0xFFFFFFFF);
+
+const kSecondaryTextColor = Color(0xFF606060);
+
+const kPrimaryStrokeColor = Color(0xFFB5B5B5);
+
+const kSecondaryStrokeColor = Color(0xFFCFCFCF);
