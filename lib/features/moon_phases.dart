eeeb495f645aa213e5f1cf5fe10a10ef9b774e82
KimJihay <eh202201029@wmsu.edu.ph>
2025-08-20 21:06:18 +0800

    add initial features of fishcast

---
+++ lib/features/moon_phases.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoonPhasesPage extends StatefulWidget {
+  const MoonPhasesPage({super.key});
+
+  @override
+  State<MoonPhasesPage> createState() => _MoonPhasesPageState();
+}
+
+class _MoonPhasesPageState extends State<MoonPhasesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
